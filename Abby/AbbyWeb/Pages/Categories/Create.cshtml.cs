233dfe4a1c7c03a994914aa1cad997a10a858952
Adesha H P <Adesha.Hp@Solera.com>
2023-01-23 10:10:15 +0530

    Add project files.

---
+++ Abby/AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,29 @@
+using AbbyWeb.Data;
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories
+{
+    [BindProperties]
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost(Category category)
+        {
+            await _db.Category.AddAsync(category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}
