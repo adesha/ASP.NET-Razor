89d1cb3c9c93c6442ae7b6ed714464ffb890cb28
Adesha H P <Adesha.Hp@Solera.com>
2023-01-24 09:55:07 +0530

    Adding Edit and Delete

---
+++ Abby/AbbyWeb/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,34 @@
+using AbbyWeb.Data;
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories
+{
+    [BindProperties]
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet(int id)
+        {
+            Category = _db.Category.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Remove(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
